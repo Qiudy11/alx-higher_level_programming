84b54bbe947b94a0486888cbd557462c43f75ec9
Muyiwa <muyiwasalau@gmail.com>
2021-09-06 14:24:24 -0700

    create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-new = str[39:66]
-str = new + " " + 'with Python'
+str = str[39:66] + " " + 'with Python'
 print(str)
