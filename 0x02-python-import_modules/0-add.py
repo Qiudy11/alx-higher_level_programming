594972be227819f75026b444b3bab851705fcc3d
Muyiwa <muyiwasalau@gmail.com>
2021-09-11 01:49:05 -0700

    functions to add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import add_0
-
-a = 1
-b = 2
-add = add_0.add(a, b)
-print("{} + {} = {}".format(a , b, add))
+import add_0 as add
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a , b, add.add(a, b)))
       
