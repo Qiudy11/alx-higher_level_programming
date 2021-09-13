b1f0810d5c769c6780d62cce195470dcb2378da5
Muyiwa <muyiwasalau@gmail.com>
2021-09-13 14:37:12 -0700

    removed white space

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,6 @@ def max_integer(my_list=[]):
         for x in my_list:
             if x > max:
                 max = x
-                return max
-        
+        return max
     else:
         return None
