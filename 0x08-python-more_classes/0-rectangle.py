61d220ca2f721db75e56f98af685ecb0b5e4aea1
muyiwa <muyiwasalau@gmail.com>
2021-10-04 07:31:02 -0700

    another commit

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
 Module 0-rectangle
-Defines class Rectangle
+
 """
 
+
 class Rectangle:
-    """creates an empty class Rectangle"""
+    """
+    creates an empty class Rectangle
+    """
     pass
